e70e632f182b3aa17f39c3597e675cc78eb4ecba
nkn-ms <nkn-ms@hotmail.co.jp>
2020-02-14 19:17:22 +0900

    update

--- components/organisms/BookDetails/index.vue
+++ components/organisms/BookDetails/index.vue
@@ -1,40 +1,21 @@
 <template>
-  <v-container class="grey lighten-5">
-    <v-row>
-      <v-col cols="4">
-        <v-card class="pa-2" outlined tile>
-          <v-list-item-avatar :tile="true" height="100px" width="100px">
-            <v-img :src="value.smallImageUrl" height="100%" contain></v-img>
-          </v-list-item-avatar>
-        </v-card>
-      </v-col>
-      <v-col>
-        <v-card class="pa-2" outlined tile>
-          <v-list-item-content>
-            <v-list-item-title v-text="value.title"></v-list-item-title>
-            <v-list-item-subtitle
-              v-text="value.author"
-              class="text--primary"
-            ></v-list-item-subtitle>
-            <v-list-item-subtitle
-              v-text="value.subTitle"
-            ></v-list-item-subtitle>
-            <v-list-item-subtitle
-              v-text="`${value.itemPrice}円`"
-            ></v-list-item-subtitle>
-            <v-list-item-subtitle
-              v-text="value.salesDate"
-            ></v-list-item-subtitle>
-            <v-list-item-subtitle v-text="value.size"></v-list-item-subtitle>
-            <v-list-item-subtitle
-              v-text="value.publisherName"
-            ></v-list-item-subtitle>
-            <v-list-item-subtitle v-text="value.isbn"></v-list-item-subtitle>
-          </v-list-item-content>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-layout justify-center>
+    <v-card class="d-flex flex-no-wrap justify-center">
+      <div>
+        <v-card-title v-text="value.title" class="headline"></v-card-title>
+        <v-card-subtitle v-text="value.author"></v-card-subtitle>
+        <v-card-text v-text="value.subTitle" class="py-0"></v-card-text>
+        <v-card-text v-text="value.salesDate" class="py-0"></v-card-text>
+        <v-card-text v-text="value.size" class="py-0"></v-card-text>
+        <v-card-text v-text="value.publisherName" class="py-0"></v-card-text>
+        <v-card-text v-text="`${value.itemPrice}円`" class="py-0"></v-card-text>
+        <v-card-text v-text="value.isbn" class="py-0"></v-card-text>
+      </div>
+      <v-avatar class="ma-3" size="250" tile>
+        <v-img :src="value.largeImageUrl" contain></v-img>
+      </v-avatar>
+    </v-card>
+  </v-layout>
 </template>
 
 <script>
